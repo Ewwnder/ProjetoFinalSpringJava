2de5e0278560b968ae7eb214420b91b15af63cf6
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-06-29 14:52:56 -0300

    Criando Arquivo

---
+++ src/main/java/br/gov/fatec/itu/telefonica/agenda/projetofinaljavaspring/ProjetofinaljavaspringApplication.java
@@ -0,0 +1,13 @@
+package br.gov.fatec.itu.telefonica.agenda.projetofinaljavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofinaljavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofinaljavaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/fatec/itu/telefonica/agenda/projetofinaljavaspring/ProjetofinaljavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.fatec.itu.telefonica.agenda.projetofinaljavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinaljavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
